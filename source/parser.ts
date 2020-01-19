30ad6301eb21ba254c2cfaa9364e64661ec6bcc0
loading <49134864+load1n9@users.noreply.github.com>
2020-01-18 20:25:26 -0500

    Update parser.ts

--- source/parser.ts
+++ source/parser.ts
@@ -322,11 +322,11 @@ export function parse(tokens: Token[]): Program {
 
         const statements = [];
 
-        while (!is({ type: "keyword", value: "end" })) {
+        while (!is({ type: "keyword", value: "***" })) {
             statements.push(parseExpression());
         }
 
-        eat({ type: "keyword", value: "end" });
+        eat({ type: "keyword", value: "***" });
 
         return new FunctionDeclarationStatement({
             exported: exported,
@@ -369,7 +369,7 @@ export function parse(tokens: Token[]): Program {
         const condition = parseExpression();
         const thenStatements = [];
 
-        while (!is({ type: "keyword", value: "*" })) {
+        while (!is({ type: "keyword", value: "***" })) {
             thenStatements.push(parseExpression());
         }
 
@@ -378,7 +378,7 @@ export function parse(tokens: Token[]): Program {
         if (is({ type: "keyword", value: "else" })) {
             eat({ type: "keyword", value: "else" });
 
-            while (!is({ type: "keyword", value: "*" })) {
+            while (!is({ type: "keyword", value: "***" })) {
                 elseStatements.push(parseExpression());
             }
         }

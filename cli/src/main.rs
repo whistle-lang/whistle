5684b7b25dff5d58926bb7d4a8a7fcd3ca829f92
eliassjogreen <eliassjogreen1@gmail.com>
2020-08-08 17:51:10 +0200

    chore: :x: remove outdated examples

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,9 @@
 use whistle_core::lexer::*;
 
 fn main() {
-  let lexer = Lexer::new("fun asd(123, 123.123)".to_string());
+  let lexer = Lexer::new("export fun add(a: i32, a: i32): i32 {
+    return a + b
+  }".to_string());
 
   for tok in lexer {
     println!("{:?}", tok);

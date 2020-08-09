fced104bb3aa81cbb979d6a7c63ba1f8a32fe4ce
eliassjogreen <eliassjogreen1@gmail.com>
2020-08-09 22:21:09 +0200

    feat: :factory: added tests to the tokenizer

--- core/src/lexer/tokenizer.rs
+++ core/src/lexer/tokenizer.rs
@@ -133,3 +133,138 @@ impl Tokenizer {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::Tokenizer;
+
+  #[test]
+  fn within_index() {
+    let tokenizer = Tokenizer::new("a");
+
+    assert!(tokenizer.within_index(0));
+    assert!(!tokenizer.within_index(1));
+  }
+
+  #[test]
+  fn within_offset() {
+    let mut tokenizer = Tokenizer::new("a");
+    
+    assert!(tokenizer.within_offset(0));
+    assert!(!tokenizer.within_offset(1));
+    tokenizer.index += 1;
+    assert!(!tokenizer.within_offset(0));
+    assert!(!tokenizer.within_offset(1));
+  }
+
+  #[test]
+  fn within() {
+    let mut tokenizer = Tokenizer::new("a");
+    
+    assert!(tokenizer.within());
+    tokenizer.index += 1;
+    assert!(!tokenizer.within());
+  }
+
+  #[test]
+  fn peek_index() {
+    let tokenizer = Tokenizer::new("abc");
+    
+    assert_eq!(tokenizer.peek_index(0), Some('a'));
+    assert_eq!(tokenizer.peek_index(1), Some('b'));
+    assert_eq!(tokenizer.peek_index(2), Some('c'));
+    assert_eq!(tokenizer.peek_index(3), None);
+  }
+
+  #[test]
+  fn peek_offset() {
+    let mut tokenizer = Tokenizer::new("abc");
+    
+    assert_eq!(tokenizer.peek_offset(0), Some('a'));
+    assert_eq!(tokenizer.peek_offset(1), Some('b'));
+    assert_eq!(tokenizer.peek_offset(2), Some('c'));
+    assert_eq!(tokenizer.peek_offset(3), None);
+    tokenizer.index += 1;
+    assert_eq!(tokenizer.peek_offset(0), Some('b'));
+    assert_eq!(tokenizer.peek_offset(1), Some('c'));
+    assert_eq!(tokenizer.peek_offset(2), None);
+    assert_eq!(tokenizer.peek_offset(3), None);
+  }
+
+  #[test]
+  fn peek() {
+    let mut tokenizer = Tokenizer::new("abc");
+    
+    assert_eq!(tokenizer.peek(), Some('a'));
+    tokenizer.index += 1;
+    assert_eq!(tokenizer.peek(), Some('b'));
+    tokenizer.index += 1;
+    assert_eq!(tokenizer.peek(), Some('c'));
+    tokenizer.index += 1;
+    assert_eq!(tokenizer.peek(), None);
+  }
+
+  #[test]
+  fn is_char() {
+    let mut tokenizer = Tokenizer::new("a");
+    
+    assert!(tokenizer.is_char('a'));
+    tokenizer.index += 1;
+    assert!(!tokenizer.is_char('a'));
+  }
+
+  #[test]
+  fn is_str() {
+    let mut tokenizer = Tokenizer::new("abc");
+    
+    assert!(tokenizer.is_str("abc"));
+    assert!(!tokenizer.is_str("abcd"));
+    tokenizer.index += 1;
+    assert!(tokenizer.is_str("bc"));
+    assert!(!tokenizer.is_str("abc"));
+  }
+
+  #[test]
+  fn eat_char() {
+    let mut tokenizer = Tokenizer::new("ab");
+    
+    assert_eq!(tokenizer.eat_char('b'), None);
+    assert_eq!(tokenizer.eat_char('a'), Some('a'));
+    assert_eq!(tokenizer.eat_char('a'), None);
+    assert_eq!(tokenizer.eat_char('b'), Some('b'));
+  }
+  
+  #[test]
+  fn eat_str() {
+    let mut tokenizer = Tokenizer::new("abcd");
+    
+    assert_eq!(tokenizer.eat_str("cd"), None);
+    assert_eq!(tokenizer.eat_str("ab"), Some(String::from("ab")));
+    assert_eq!(tokenizer.eat_str("ef"), None);
+    assert_eq!(tokenizer.eat_str("cd"), Some(String::from("cd")));
+  }
+
+  #[test]
+  fn step() {
+    let mut tokenizer = Tokenizer::new("ab");
+    
+    assert_eq!(tokenizer.index, 0);
+    assert_eq!(tokenizer.step(), Some('a'));
+    assert_eq!(tokenizer.index, 1);
+    assert_eq!(tokenizer.step(), Some('b'));
+    assert_eq!(tokenizer.index, 2);
+    assert_eq!(tokenizer.step(), None);
+    assert_eq!(tokenizer.index, 2);
+  }
+
+  #[test]
+  fn read_while() {
+    let mut tokenizer = Tokenizer::new("aaaabbbb");
+    
+    assert_eq!(tokenizer.read_while(|ch| ch == 'a'), Some(String::from("aaaa")));
+    assert_eq!(tokenizer.read_while(|ch| ch == 'a'), None);
+    assert_eq!(tokenizer.read_while(|ch| ch == 'b'), Some(String::from("bbbb")));
+    assert_eq!(tokenizer.read_while(|ch| ch == 'b'), None);
+  }
+}
+ 

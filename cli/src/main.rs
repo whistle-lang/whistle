5b28f8680853d86f6887f95d0eecd3fba1b0a1f8
eliassjogreen <eliassjogreen1@gmail.com>
2020-11-10 14:18:17 +0100

    feat: output option for lexing and parsing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -33,6 +33,12 @@ fn main() {
         .help("Pretty print the tokens/program"),
     )
     .arg(
+      Arg::with_name("output")
+        .takes_value(true)
+        .short("o")
+        .help("Output the result to a file"),
+    )
+    .arg(
       Arg::with_name("file")
         .help("Sets the input file to use")
         .required(true),
@@ -44,6 +50,12 @@ fn main() {
       Arg::with_name("pretty")
         .short("p")
         .help("Pretty print the tokens/program"),
+    )
+    .arg(
+      Arg::with_name("output")
+        .takes_value(true)
+        .short("o")
+        .help("Output the result to a file"),
     )
     .arg(
       Arg::with_name("file")
@@ -69,49 +81,56 @@ fn main() {
     .subcommand(parse_option)
     .get_matches();
 
-  if let Some(command) = &app.subcommand_name() {
-    if let Some(text) = readfile(&app, command) {
-      match *command {
-        "lex" => lex(&*text),
-        "parse" => parse(&*text),
+  if let Some(command) = app.subcommand_name() {
+    if let Some(matches) = app.subcommand_matches(command) {
+      let file = matches
+        .value_of("file")
+        .expect("This argument can't be empty, we said it was required.");
+      let text = fs::read_to_string(file).expect("Something went wrong, we can't read this file.");
+      let output = matches.value_of("output");
+
+      match command {
+        "lex" => lex(&text, output),
+        "parse" => parse(&text, output),
         _ => println!("Unreachable"),
       };
     }
   }
 }
 
-fn readfile(app: &clap::ArgMatches, command: &str) -> Option<String> {
-  if let Some(matches) = app.subcommand_matches(command) {
-    let file = matches
-      .value_of("file")
-      .expect("This argument can't be empty, we said it was required.");
-    let text = fs::read_to_string(file).expect("Something went wrong, we can't read this file.");
-    Some(text)
-  } else {
-    None
-  }
-}
-
-fn lex(text: &str) {
+fn lex(text: &str, output: Option<&str>) {
   let lexer = Lexer::new(text);
   let now = Instant::now();
+  let mut toks = Vec::new();
   for tok in lexer {
-    println!("{:?}", tok);
+    toks.push(tok.clone());
+
     if tok.is_err() {
       break;
     }
   }
+
+  if let Some(file) = output {
+    fs::write(file, format!("{:#?}", toks))
+      .expect("Something went wrong, we can't write this file.");
+  }
+
   println!(
     "Operation complete! Took us about {} seconds.",
     now.elapsed().as_secs_f64()
   );
 }
 
-fn parse(text: &str) {
+fn parse(text: &str, output: Option<&str>) {
   let lexer = Lexer::new(text);
   let mut parser = Parser::from(lexer);
   let now = Instant::now();
-  parse_grammar(&mut parser);
+  let res = parse_grammar(&mut parser);
+
+  if let Some(file) = output {
+    fs::write(file, format!("{:#?}", res)).expect("Something went wrong, we can't write this file.");
+  }
+
   println!(
     "Operation complete! Took us about {} seconds.",
     now.elapsed().as_secs_f64()
